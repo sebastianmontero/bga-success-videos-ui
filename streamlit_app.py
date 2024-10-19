2573b78c0570d6ae30b2d1f233ec974dee2ec4d3
sebastianmontero <sebastian.montero@gmail.com>
2024-10-19 00:18:13 +0000

    Started developing the main ui

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("BGA Testimonios")
+
+text_search = st.text_input("Busca testimonios por caso de uso", value="")
+industry = st.selectbox("Industria", ["", "Agricultura", "Finanzas"])
