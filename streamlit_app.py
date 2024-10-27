3d87f0d9b686d5a8239d275b16924aa06231480a
Sebastian Montero <sebastian.montero@gmail.com>
2024-10-26 21:32:05 -0600

    Developed the app to show the vimeo video as part of the results.

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,10 @@ def industries() -> dict:
 
 st.title("BGA Testimonios")
 
+# st.markdown("""
+# <iframe src="https://player.vimeo.com/video/995195350" width="400px" height="225px" frameborder="0"></iframe>
+# """, unsafe_allow_html=True)
+
 with st.form("search"):
   search_text = st.text_input("Busca testimonios por caso de uso", value="")
   industry = st.selectbox("Industria", industries(), index=None)
@@ -28,7 +32,7 @@ if search_text:
     results = sv_api.search_success_videos(search_text, int(num_results), industry)
     print(results)
 
-N_cards_per_row = 3
+N_cards_per_row = 2
 if search_text:
     for n_row, row in enumerate(results):
         metadata = row['metadata']
@@ -38,9 +42,23 @@ if search_text:
             cols = st.columns(N_cards_per_row, gap="large")
         # draw the card
         with cols[n_row%N_cards_per_row]:
-            st.caption(f"{metadata['industry'].strip()}")
-            st.markdown(f"**{metadata['title'].strip()}**")
-            st.markdown(f"*{metadata['client'].strip()}*")
-            st.caption(f"{row['page_content'].strip()}")
-            st.markdown(f"**http://bga.com/{metadata['source']}**")
-            st.caption(", ".join(metadata['speakers']))
+                st.caption(f"{metadata['industry'].strip()}")
+                st.markdown(f"**{metadata['title'].strip()}**")
+                st.markdown(f"*{metadata['client'].strip()}*")
+                st.caption(f"{row['page_content'].strip()}")
+                st.markdown(f"**http://bga.com/{metadata['source']}**")
+                st.caption(", ".join(metadata['speakers']))
+                
+                # Video iframe
+                st.markdown(
+                    f"""<iframe 
+                        src="https://player.vimeo.com/video/{metadata['video_id']}?title=0&byline=0&portrait=0" 
+                        width="100%" 
+                        height="225px" 
+                        frameborder="0"
+                        style="margin-top: 1rem;"
+                        allow="autoplay; fullscreen; picture-in-picture" 
+                        allowfullscreen
+                    ></iframe>""", 
+                    unsafe_allow_html=True
+                )
